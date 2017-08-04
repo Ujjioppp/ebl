2600c2049cd5e7960116ecce148940a8d867d093
finalye <3253942@qq.com>
2017-08-04 09:47:50 +0800

    test case

--- src/test/java/org/ebl/EblApplicationTests.java
+++ src/test/java/org/ebl/EblApplicationTests.java
@@ -1,13 +1,7 @@
 package org.ebl;
 
-import com.github.pagehelper.Page;
-import com.github.pagehelper.PageHelper;
-import org.ebl.entity.Corporation;
-import org.ebl.entity.CorporationExample;
-import org.ebl.mapper.CorporationMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -15,15 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class EblApplicationTests {
 
-	@Autowired
-	private CorporationMapper corporationMapper;
-
 	@Test
 	public void contextLoads() {
-		PageHelper.startPage(1,1);
-		CorporationExample example = new CorporationExample();
-		Page<Corporation> obj = (Page<Corporation>)this.corporationMapper.selectByExample(example);
-		System.err.println(obj.getTotal());
 	}
 
 }
